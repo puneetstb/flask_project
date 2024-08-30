602da56b2fda8ee4d7a35d538f60aedb4bd68ef7
puneetstb <puneetarts.stb@gmail.com>
2024-08-30 13:31:55 +0530

    update changes

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user, login_required
+from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy.exc import IntegrityError  # Import IntegrityError
 from datetime import datetime
 from werkzeug.utils import secure_filename
 
@@ -8,10 +11,13 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///recipes.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['UPLOAD_FOLDER'] = 'static/uploads'
+app.config['SECRET_KEY'] = 'your_secret_key'
 
 db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     email = db.Column(db.String(150), unique=True, nullable=False)
@@ -33,30 +39,86 @@ class Recipe(db.Model):
     def __repr__(self):
         return f'<Recipe {self.title}>'
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 @app.route('/')
 @app.route('/index')
 def index():
     recipes = Recipe.query.all()
     return render_template('index.html', recipes=recipes)
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        
+        # Check if the email already exists
+        existing_user = User.query.filter_by(email=email).first()
+        if existing_user:
+            flash('Email address already in use. Please choose a different one.', 'danger')
+            return redirect(url_for('register'))
+        
+        hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
+        new_user = User(username=username, email=email, password=hashed_password)
+        
+        try:
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Registration successful! You can now log in.', 'success')
+            return redirect(url_for('login'))
+        except IntegrityError:
+            db.session.rollback()
+            flash('An error occurred during registration. Please try again.', 'danger')
+            return redirect(url_for('register'))
+        
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        user = User.query.filter_by(email=email).first()
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            flash('Login successful!', 'success')
+            return redirect(url_for('index'))
+        else:
+            flash('Login failed. Check your email and password.', 'danger')
+            return redirect(url_for('login'))
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out.', 'info')
+    return redirect(url_for('index'))
+
 @app.route('/create', methods=['GET', 'POST'])
+@login_required
 def create():
     if request.method == 'POST':
         title = request.form['title']
         ingredients = request.form['ingredients']
         instructions = request.form['instructions']
-        user_id = request.form['user_id']
+        user_id = current_user.id
 
         if 'image' not in request.files:
+            flash('No image uploaded.', 'danger')
             return redirect(request.url)
+        
         file = request.files['image']
         if file.filename == '':
+            flash('No selected image.', 'danger')
             return redirect(request.url)
+        
         if file:
             filename = secure_filename(file.filename)
-            # Ensure the upload folder exists
-            if not os.path.exists(app.config['UPLOAD_FOLDER']):
-                os.makedirs(app.config['UPLOAD_FOLDER'])
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
         new_recipe = Recipe(
@@ -71,7 +133,49 @@ def create():
         return redirect(url_for('index'))
     return render_template('create.html')
 
+# @app.route('/update/<int:id>', methods=['GET', 'POST'])
+# @login_required
+# def update(id):
+    recipe = Recipe.query.get_or_404(id)
+    if request.method == 'POST':
+        recipe.title = request.form['title']
+        recipe.ingredients = request.form['ingredients']
+        recipe.instructions = request.form['instructions']
+
+        if 'image' in request.files:
+            file = request.files['image']
+            if file and file.filename != '':
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                recipe.image = filename
+        
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('update.html', recipe=recipe)
+
+# @app.route('/update/<int:id>', methods=['GET', 'POST'])
+# @login_required
+# def update(id):
+#     recipe = Recipe.query.get_or_404(id)
+#     if request.method == 'POST':
+#         recipe.title = request.form['title']
+#         recipe.ingredients = request.form['ingredients']
+#         recipe.instructions = request.form['instructions']
+
+#         if 'image' in request.files:
+#             file = request.files['image']
+#             if file and file.filename != '':
+#                 filename = secure_filename(file.filename)
+#                 file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+#                 recipe.image = filename
+        
+#         db.session.commit()
+#         flash('Recipe updated successfully!', 'success')
+#         return redirect(url_for('index'))
+#     return render_template('update.html', recipe=recipe)
+
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
+@login_required
 def update(id):
     recipe = Recipe.query.get_or_404(id)
     if request.method == 'POST':
@@ -83,16 +187,17 @@ def update(id):
             file = request.files['image']
             if file and file.filename != '':
                 filename = secure_filename(file.filename)
-                if not os.path.exists(app.config['UPLOAD_FOLDER']):
-                    os.makedirs(app.config['UPLOAD_FOLDER'])
                 file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
                 recipe.image = filename
         
         db.session.commit()
+        flash('Recipe updated successfully!', 'success')
         return redirect(url_for('index'))
     return render_template('update.html', recipe=recipe)
 
+
 @app.route('/delete/<int:id>')
+@login_required
 def delete(id):
     recipe = Recipe.query.get_or_404(id)
     db.session.delete(recipe)
@@ -102,5 +207,4 @@ def delete(id):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  
-    app.run(debug=True, port=5001)
-
+    app.run(debug=True, port=5002)
